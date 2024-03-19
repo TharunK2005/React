169a0a7776e62d7274377805bc08bc0ffeb7b87b
TharunK2005 <163411267+TharunK2005@users.noreply.github.com>
2024-03-19 10:02:05 +0530

    Add files via upload

---
+++ data.service.js
@@ -0,0 +1,41 @@
+import axios from 'axios';
+ 
+export let dataServiceObj =
+{
+    getAllEmployees,
+    getEmpById,
+    addEmp,
+    updateEmp,
+    deleteEmp
+};
+ 
+ 
+ 
+let url = "http://localhost:3000/emps/";
+ 
+function getAllEmployees()
+{
+    return axios.get(url);
+}
+ 
+ 
+function addEmp(empObj)
+{
+    return axios.post(url, empObj);
+}
+ 
+function deleteEmp(eno)
+{
+    return axios.delete(url + eno);
+}
+ 
+ 
+function getEmpById(eno)
+{
+    return axios.get(url + eno);
+}
+ 
+function updateEmp(empObj)
+{
+    return axios.put(url + empObj.empno, empObj);
+}
